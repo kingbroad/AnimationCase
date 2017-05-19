01b4f5cdcffdf6258804b3f7100e04f17a8c5009
kingbroad <1301237459@qq.com>
2017-05-19 14:02:59 +0800

    Initial Commit

---
+++ AnimationCase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationCase
+//
+//  Created by TakumiShen on 17/5/19.
+//  Copyright © 2017年 Jinzhiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
